3b1320c32dbeed3447b13d694c35e73a8669c2ef
pablopunk <pablovarela182@gmail.com>
2019-05-22 18:50:09 +0200

    Split into two packages

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
-const fs = require('fs')
-const path = require('path')
-let colors = require('sick-colors')
+const hyperMac = require('hyper-mac')
+const hyperSick = require('sick-colors')
 
-const css = fs.readFileSync(path.join(__dirname, 'index.css'), 'utf-8')
+exports.decorateConfig = config =>
+  hyperMac.decorateConfig(hyperSick.decorateConfig(config))
 
-colors = {
-  ...colors,
-  lightBlack: colors.black,
-  lightRed: colors.red,
-  lightGreen: colors.green,
-  lightYellow: colors.yellow,
-  lightBlue: colors.blue,
-  lightMagenta: colors.magenta,
-  lightCyan: colors.cyan,
-  lightWhite: colors.foreground
-}
+exports.decorateBrowserOptions = defaults =>
+  hyperMac.decorateBrowserOptions(defaults)
 
-exports.decorateConfig = config => ({
-  ...config,
-  padding: '5px',
-  backgroundColor: colors.background,
-  foregroundColor: colors.foreground,
-  css: (config.css || '') + css,
-  colors
-})
-
-exports.decorateBrowserOptions = defaults => ({
-  ...defaults,
-  titleBarStyle: 'native',
-  transparent: false
-})
-
-exports.getTabsProps = (parentProps, props) => {
-  props.tabs.length < 2
-    ? document.body.classList.add('closed-tabs')
-    : document.body.classList.remove('closed-tabs')
-
-  return {
-    ...parentProps,
-    ...props
-  }
-}
+exports.getTabsProps = (parentProps, props) =>
+  hyperMac.getTabsProps(parentProps, props)
